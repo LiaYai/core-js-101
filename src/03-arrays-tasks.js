2dd52913b2bfb09960d52d85feb25ec82b4e70a3
Talissa <liayai@ya.ru>
2024-08-28 20:04:31 +0300

    finish array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -514,8 +514,14 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  return array.reduce(
+    (map1, item) => (map1.has(keySelector(item))
+      ? map1.set(keySelector(item), [map1.get(keySelector(item)), valueSelector(item)].flat())
+      : map1.set(keySelector(item), [valueSelector(item)])),
+    map,
+  );
 }
 
 
@@ -532,8 +538,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((item) => childrenSelector(item)).flat();
 }
 
 
@@ -549,8 +555,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((res, item) => res[item], arr);
 }
 
 
@@ -572,8 +578,11 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const newArr = arr.slice(0, Math.floor(arr.length / 2));
+  arr.copyWithin(0, -Math.floor(arr.length / 2))
+    .splice(-Math.floor(arr.length / 2), Math.floor(arr.length / 2), newArr);
+  return arr.flat();
 }
 
 
